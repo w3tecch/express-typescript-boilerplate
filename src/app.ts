d0e634facc12379378b7f844753ac3488ead96d7
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-06-26 18:25:53 +0200

    Update inline documentation

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,12 @@
  * EXPRESS TYPESCRIPT BOILERPLATE
  * ----------------------------------------
  *
- * Gery Hirscheld<@hirsch88>
- *
  * This is a boilerplate for Node.js Application written in TypeScript.
  * The basic layer of this app is express. For further information visit
  * the 'README.md' file.
  *
- * It is very important the '/core' module is loaded first, because this
- * module loads all essentials third-party-libs and our configs we need.
- *
- * To add express modules go to the 'app.ts' file. All the IOC registrations
- * are in the 'container.ts' file.
+ * To add express modules go to the 'config/AppConfig.ts' file. All the IOC registrations
+ * are in the 'config/IocConfig.ts' file.
  */
 
 import 'reflect-metadata';
