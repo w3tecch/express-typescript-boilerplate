cea55f43998806415ce11916cda38241c881b5a8
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-11-17 23:25:01 +0100

    Rename CustomHeaderConfig

--- src/config/CustomHeaderConfig.ts
+++ src/config/CustomHeaderConfig.ts
@@ -13,12 +13,12 @@ import { App, Configurable } from '../core/App';
 import { Environment } from '../core/helpers/Environment';
 
 
-export class CustomConfig implements Configurable {
+export class CustomHeaderConfig implements Configurable {
 
     private log = new Logger(__filename);
 
     public configure(app: App): void {
-        this.log.debug('configuring', app.Express.get('port'));
+        this.log.debug('Add custom headers');
 
         app.Express.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
             res.setHeader('X-API-VERSION', Environment.getPkg().version);
