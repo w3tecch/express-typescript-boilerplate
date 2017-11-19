dd412a2611640f45ba9bc468344e7e99cff4bd48
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-11-19 15:42:32 +0100

    Add banner

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@
  * This is a boilerplate for Node.js Application written in TypeScript.
  * The basic layer of this app is express. For further information visit
  * the 'README.md' file.
- *
  */
 import 'reflect-metadata';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import { banner } from './core/banner';
 import { Log } from './core/Log';
 const log = new Log(__filename);
 
@@ -26,6 +26,10 @@ import { iocLoader } from './loaders/iocLoader';
 
 
 bootstrapMicroframework({
+    /**
+     * Loader is a place where you can configure all your modules during microframework
+     * bootstrap. All loaders are executed one by one in a sequential order.
+     */
     loaders: [
         winstonLoader,
         iocLoader,
@@ -35,8 +39,7 @@ bootstrapMicroframework({
         monitorLoader,
         homeLoader,
         publicLoader
-        // here we can setup other databases, any other lib we want to setup in our application
     ]
 })
-    .then(() => log.info('Application is up and running.'))
+    .then(() => banner(log))
     .catch(error => log.error('Application is crashed: ' + error));
---
+++ src/core/banner.ts
@@ -0,0 +1,27 @@
+import { Log } from './Log';
+import { env } from './env';
+
+
+export function banner(log: Log): void {
+    if (env.app.banner) {
+        log.info(``);
+        log.info(`Aloha, your app is ready on ${env.app.route}${env.app.routePrefix}`);
+        log.info(`To shut it down, press <CTRL> + C at any time.`);
+        log.info(``);
+        log.info('-------------------------------------------------------');
+        log.info(`Environment  : ${env.node}`);
+        log.info(`Version      : ${env.app.version}`);
+        log.info(``);
+        log.info(`API Info     : ${env.app.route}${env.app.routePrefix}`);
+        if (env.swagger.enabled) {
+            log.info(`Swagger      : ${env.app.route}${env.swagger.route}`);
+        }
+        if (env.monitor.enabled) {
+            log.info(`Monitor      : ${env.app.route}${env.monitor.route}`);
+        }
+        log.info('-------------------------------------------------------');
+        log.info('');
+    } else {
+        log.info(`Application is up and running.`);
+    }
+}
