8d7c6d03baaca6e78e6695da530ada1ccc34e22d
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-12-10 16:27:02 +0100

    Fix spelling mistake

--- src/api/middlewares/CompressionMiddleware.ts
+++ src/api/middlewares/CompressionMiddleware.ts
@@ -5,7 +5,7 @@ import { ExpressMiddlewareInterface, Middleware } from 'routing-controllers';
 
 
 @Middleware({ type: 'before' })
-export class SecurityMiddleware implements ExpressMiddlewareInterface {
+export class CompressionMiddleware implements ExpressMiddlewareInterface {
 
     public use(req: express.Request, res: express.Response, next: express.NextFunction): any {
         return compression()(req, res, next);
