8035ea233891465dc57846b3e6c075a5aecb4981
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-03-15 07:45:50 +0100

    Fix seeding

--- src/database/seeds/CreatePets.ts
+++ src/database/seeds/CreatePets.ts
@@ -8,12 +8,11 @@ export class CreatePets implements Seed {
 
     public async seed(factory: Factory, connection: Connection): Promise<any> {
         const em = connection.createEntityManager();
-
         await times(10, async (n) => {
             const pet = await factory(Pet)().seed();
             const user = await factory(User)().make();
             user.pets = [pet];
-            await em.save(user);
+            return await em.save(user);
         });
     }
 
--- src/lib/seed/utils.ts
+++ src/lib/seed/utils.ts
@@ -11,5 +11,11 @@ export const isPromiseLike = (o: any): boolean => !!o && (typeof o === 'object' 
 /**
  * Times repeats a function n times
  */
-export const times = async <TResult>(n: number, iteratee: (index: number) => Promise<TResult>): Promise<TResult[]> =>
-    Promise.all(new Array(n).map(async (v, i) => await iteratee(i)));
+export const times = async <TResult>(n: number, iteratee: (index: number) => Promise<TResult>): Promise<TResult[]> => {
+    const rs = [] as TResult[];
+    for (let i = 0; i < n; i++) {
+        const r = await iteratee(i);
+        rs.push(r);
+    }
+    return rs;
+};
