e05dcf3a103d335ee77be82be0aea6da6340253d
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-05-14 09:13:12 +0200

    :art: Formats the controllers

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -12,33 +12,31 @@ export class UserController {
 
     constructor(
         private userService: UserService
-    ) {
-        console.log('UserControllerUserControllerUserController');
-    }
+    ) { }
 
     @Get()
-    public find( @CurrentUser() user?: User): Promise<User[]> {
+    public find(@CurrentUser() user?: User): Promise<User[]> {
         return this.userService.find();
     }
 
     @Get('/:id')
     @OnUndefined(UserNotFoundError)
-    public one( @Param('id') id: string): Promise<User | undefined> {
+    public one(@Param('id') id: string): Promise<User | undefined> {
         return this.userService.findOne(id);
     }
 
     @Post()
-    public create( @Body() user: User): Promise<User> {
+    public create(@Body() user: User): Promise<User> {
         return this.userService.create(user);
     }
 
     @Put('/:id')
-    public update( @Param('id') id: string, @Body() user: User): Promise<User> {
+    public update(@Param('id') id: string, @Body() user: User): Promise<User> {
         return this.userService.update(id, user);
     }
 
     @Delete('/:id')
-    public delete( @Param('id') id: string): Promise<void> {
+    public delete(@Param('id') id: string): Promise<void> {
         return this.userService.delete(id);
     }
 
