5d9e25950b4dafc89fd0d0dfc470406809a78cec
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-05-15 07:51:43 +0200

    :sparkles: Change to swagger 3.0.0

--- src/loaders/swaggerLoader.ts
+++ src/loaders/swaggerLoader.ts
@@ -16,9 +16,12 @@ export const swaggerLoader: MicroframeworkLoader = (settings: MicroframeworkSett
             description: env.app.description,
             version: env.app.version,
         };
-        swaggerFile.host = `${env.app.host}:${env.app.port}`;
-        swaggerFile.basePath = env.app.routePrefix;
-        swaggerFile.schemes = [env.app.schema];
+
+        swaggerFile.servers = [
+            {
+                url: `${env.app.schema}://${env.app.host}:${env.app.port}${env.app.routePrefix}`,
+            },
+        ];
 
         expressApp.use(
             env.swagger.route,
