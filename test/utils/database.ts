38d1555113b6957cd54e6119c2ad5bb3c49b4a9f
Maxim Zinovik <maxim.zinovik@gmail.com>
2018-11-23 15:35:55 +0300

    Fix typeorm logging option

--- test/utils/database.ts
+++ test/utils/database.ts
@@ -3,12 +3,14 @@ import { Connection, createConnection, useContainer } from 'typeorm';
 
 import { env } from '../../src/env';
 
+declare type LoggerOptions = boolean | 'all' | Array<('query' | 'schema' | 'error' | 'warn' | 'info' | 'log' | 'migration')>;
+
 export const createDatabaseConnection = async (): Promise<Connection> => {
     useContainer(Container);
     const connection = await createConnection({
         type: env.db.type as any, // See createConnection options for valid types
         database: env.db.database,
-        logging: env.db.logging,
+        logging: env.db.logging as LoggerOptions,
         entities: env.app.dirs.entities,
         migrations: env.app.dirs.migrations,
     });
