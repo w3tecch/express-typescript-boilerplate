ae87d5440349000cea9b2d3548209267e1680007
Mario Gualtieri <mario.gualtieri2012@gmail.com>
2019-01-03 16:35:51 +0100

    Better swaggerFile content factory

--- src/loaders/swaggerLoader.ts
+++ src/loaders/swaggerLoader.ts
@@ -11,29 +11,27 @@ import { env } from '../env';
 export const swaggerLoader: MicroframeworkLoader = (settings: MicroframeworkSettings | undefined) => {
     if (settings && env.swagger.enabled) {
         const expressApp = settings.getData('express_app');
-        const routingMetadataStorage: MetadataArgsStorage = getMetadataArgsStorage();
-        const swaggerFile: any = routingControllersToSpec(routingMetadataStorage);
-
         const validationMetadatas = (getFromContainer(MetadataStorage) as any).validationMetadatas;
         const schemas = validationMetadatasToSchemas(validationMetadatas, {
             refPointerPrefix: '#/components/schemas',
         });
 
-        // Apply schema
-        swaggerFile.components = schemas;
-
-        // Add npm infos to the swagger doc
-        swaggerFile.info = {
-            title: env.app.name,
-            description: env.app.description,
-            version: env.app.version,
-        };
-
-        swaggerFile.servers = [
-            {
-                url: `${env.app.schema}://${env.app.host}:${env.app.port}${env.app.routePrefix}`,
+        const routingMetadataStorage: MetadataArgsStorage = getMetadataArgsStorage();
+        const swaggerFile: any = routingControllersToSpec(routingMetadataStorage, {}, {
+            servers: [
+                {
+                    url: `${env.app.schema}://${env.app.host}:${env.app.port}${env.app.routePrefix}`,
+                },
+            ],
+            info: {
+                title: env.app.name,
+                description: env.app.description,
+                version: env.app.version,
             },
-        ];
+            components: {
+                schemas,
+            },
+        });
 
         expressApp.use(
             env.swagger.route,
